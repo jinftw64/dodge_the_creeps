8caba6f9dba1b404685f766b5c8d863336cb51d0
jin <jin@pop-os.localdomain>
2024-08-07 23:20:33 -0400

    fix bug with mob sprites not despawning when leaving the screen

--- Main.gd
+++ Main.gd
@@ -6,7 +6,7 @@ var score
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	new_game()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- mob.gd
+++ mob.gd
@@ -14,7 +14,3 @@ func _process(delta):
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
-
-
-func _on_visible_on_screen_enabler_2d_screen_exited():
-	pass # Replace with function body.

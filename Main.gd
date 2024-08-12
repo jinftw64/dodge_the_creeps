6ecf622cac61cd456587d6423a5cad66f9389b52
jin <103798287+jinftw64@users.noreply.github.com>
2024-08-12 15:38:34 -0400

    implement HUD and queue_free mobs group when new_game is called

--- Main.gd
+++ Main.gd
@@ -6,7 +6,7 @@ var score
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	new_game()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,11 +18,18 @@ func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	
+	$HUD.show_game_over()
+	
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	
+	get_tree().call_group("mobs", "queue_free")
 
 
 func _on_start_timer_timeout():
@@ -32,6 +39,8 @@ func _on_start_timer_timeout():
 
 func _on_score_timer_timeout():
 	score += 1
+	
+	$HUD.update_score(score)
 
 
 func _on_mob_timer_timeout():
